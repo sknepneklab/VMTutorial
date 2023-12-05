ecc61bb188d032605c4c511681ddeb21d75af994
Rastko Sknepnek <sknepnek@gmail.com>
2023-12-05 18:59:38 +0000

    Added Python file for plotting with PyVista

---
+++ examples/RandomRelax/tissue_plot.py
@@ -0,0 +1,86 @@
+try:
+    import pyvista as pv
+except ImportError:
+    raise ImportError('PyVista needs to be installed before tissue_plot can be used.')
+
+import numpy as np 
+import matplotlib
+from VMToolkit.VM import *
+
+def rgb_int2tuple(rgbint):
+    return [int(rgbint[0]*256),  int(rgbint[1]*256), int(rgbint[2]*256)]
+
+def make_plotter(tissue, cmap = 'hot', colourby = 'area'):
+    vertices = []
+    faces = []
+    colours = []
+
+    box = tissue.vertices()[0].r.box()
+    Lx = box[0]
+    Ly = box[-1]
+
+    for v in tissue.vertices():
+        vert = [v.r.x, v.r.y, 0]
+        vertices.append(vert)
+
+    quantity = []
+    for (i,c) in enumerate(tissue.cells()):
+        verts = [c.neighbours]
+        rc = tissue.get_cell_centroid(i)
+        omit = False
+        for he in FaceCirculator(c):
+            vto = he.vto()
+            if abs(vto.r.x - rc.x) >= 0.5*Lx or abs(vto.r.y - rc.y) >= 0.5*Ly:
+                omit = True 
+                break
+            verts.append(he.vto().id)
+        if not omit:
+            faces.append(verts)
+            if colourby == 'area':
+                quantity.append(tissue.cell_area(i))
+            elif colourby == 'perim':
+                quantity.append(tissue.cell_perim(i))
+            else:
+                raise Exception(f'Unknown quantity {colourby}.')
+    
+    faces = np.hstack(faces)
+    quantity = np.array(quantity)
+
+    norm = matplotlib.colors.Normalize(vmin=np.min(quantity), vmax=np.max(quantity))
+    cm = matplotlib.colormaps[cmap]
+
+    for q in quantity:
+        colour = cm(norm(q))
+        colours.append(list(colour))
+
+    mesh = pv.PolyData(vertices, faces)
+    mesh.cell_data['colors'] = colours
+    mesh.cell_data[colourby] = quantity
+    
+    plotter = pv.Plotter()
+    _ = plotter.add_mesh(
+        mesh,
+        scalars='colors',
+        lighting=False,
+        rgb=True,
+        preference='cell',
+        show_edges=True,
+        line_width = 1,
+        scalar_bar_args={'title': colourby},
+        show_scalar_bar=False,
+    )
+    _ = plotter.add_scalar_bar(
+        colourby,
+        interactive=False,
+        vertical=False,
+        title_font_size=16,
+        label_font_size=16,
+        outline=False,
+        fmt='%6.2f',
+    )
+    plotter.camera_position = 'xy'
+    return plotter
+
+
+
+    
